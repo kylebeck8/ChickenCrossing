534855f87edad521b65e3f45d1f4aa76c09264df
Kyle Beck <kyle.d.beck.1@purdue.edu>
2018-12-06 00:24:36 -0500

    Cars spawn on roads automatically

--- Backend/sketch.js
+++ Backend/sketch.js
@@ -1,9 +1,9 @@
-var xCor = 42;
+var xCor = 40;
 var yCor;
 var driveUp;
 var driveDown;
 var cars = [];
-var gameMap = [0, 0, 9, 0, 9, 9, 0, 9, 9, 9, 0];
+var gameMap = [1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1];
 var chicken;
 var scoreElem;
 var maxScore = 0;
@@ -11,7 +11,7 @@ var widthElement;
 var mapXPos = 0;
 
 function preload() {
-  chickenPic = loadImage('assets/chicken.jpg');
+  chickenPic = loadImage('assets/chicken.png');
   carDownPic = loadImage('assets/car_down.png');
   carUpPic = loadImage('assets/car_up.png');
   roadPic = loadImage('assets/road.png');
@@ -32,10 +32,10 @@ function setup() {
   widthElement = roadPic.width/2;
   mapXPos = widthElement * 11;
 
-  chicken = createSprite(xCor, yCor);
+  chicken = createSprite(xCor, yCor, chickenPic.width * 1.5, chickenPic.height * 1.5);
   chicken.addImage(chickenPic);
 
-  for(var i = 0; i < 30; i++) {
+  /*for(var i = 0; i < 30; i++) {
     cars.push(new Driver(i));
     cars[i] = createSprite(cars[i].x, cars[i].y);
     if(cars[i].position.x % 100 == 0) {
@@ -45,7 +45,7 @@ function setup() {
       cars[i].addImage(carDownPic);
     }
 
-  }
+  } */
 
   camera.position.x = chicken.position.x + 250;
   camera.position.y = chicken.position.y;
@@ -63,23 +63,31 @@ function draw() {
     //console.log(width);
     //console.log(mapXPos);
     for (var i=0; i<2; i++) {
-      gameMap.push(int(random(1,11)));
+      var tile = int(random(1,11))
+      if (gameMap[gameMap.length - 1] == 1) {
+        gameMap.push(0);
+        AddCars();
+      } else if (tile < 4) {
+        gameMap.push(1);
+      } else {
+        gameMap.push(0);
+        AddCars();
+      }
       mapXPos += widthElement;
     }
   }
   //display map
   for (var i=0; i<gameMap.length; i++) {
-    if (gameMap[i] > 4 || ((i > 1) && gameMap[i - 1] < 5)) {
+    if (gameMap[i] == 0) {
       DisplayMapElement(0, widthElement * i);
-      //spawn car on road
-    } else {
+    } else if (gameMap[i] == 1) {
       DisplayMapElement(1, widthElement * i);
     }
   }
 
   drawSprites();
 
-  camera.position.x += 1 /*chicken.position.x + 250*/;
+  camera.position.x += 3 /*chicken.position.x + 250*/;
 
   //moves cars
   for (var i=0; i<cars.length; i++) {
@@ -107,9 +115,18 @@ function draw() {
 
   //check score
   if(chicken.position.x > maxScore) {
-    maxScore = chicken.position.x - 42;
+    maxScore = chicken.position.x - 40;
     scoreElem.html('Score = ' + maxScore / (widthElement / 2));
   }
+}
+
+function AddCars() {
+  cars.push(new Driver(cars.length, mapXPos+widthElement/4));
+  cars[cars.length-1] = createSprite(cars[cars.length-1].x, cars[cars.length-1].y);
+  cars[cars.length-1].addImage(carDownPic);
+  cars.push(new Driver(cars.length, mapXPos+widthElement*3/4));
+  cars[cars.length-1] = createSprite(cars[cars.length-1].x, cars[cars.length-1].y);
+  cars[cars.length-1].addImage(carDownPic);
 }
 
 function DisplayMapElement(element, location) {
@@ -128,15 +145,14 @@ function DisplayMapElement(element, location) {
   }
 }
 
-function Driver(id) {
-  var x = floor(random(51, width))
-  this.x = x + (50 - (x % 50));
+function Driver(id, pos) {
+  this.x = pos;
 
   if(this.x % 100 == 0) {
-    this.y = driveUp + (id * 130);
+    this.y = driveUp;
   }
   else {
-    this.y = driveDown - (id * 130);
+    this.y = driveDown;
   }
 }
 
@@ -149,11 +165,10 @@ function keyPressed() {
       chicken.position.x += widthElement/2;
       break;
     case 83: //up
-      chicken.position.y += widthElement/2;
+      chicken.position.y += 64;
       break;
     case 87: //down
-      chicken.position.y -= widthElement/2;
+      chicken.position.y -= 64;
       break;
   }
-  return false;
 }
